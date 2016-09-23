eeedb64da8c2d9b72b0f9e3a1c547b982b17c7c5
Ricardo Oyarzun <royarzun@gmail.com>
2016-09-22 21:14:37 -0400

    Add find active DPEs feature

--- clara/subscriber.js
+++ clara/subscriber.js
@@ -1,15 +1,22 @@
+const zmq = require('zmq'),
+    sock = zmq.socket('sub');
+let set = new Set();
 
-
-const zmq = require('zmq'), sock = zmq.socket('sub');
 
 try {
-  sock.connect('tcp://129.57.114.188:7772');
-  sock.subscribe('dpeAlive');
-  console.log('Subscriber connected to clara SUB port: 7772');
+    var myip = require('quick-local-ip');
+    sock.connect('tcp://' + myip.getLocalIP4() + ':7772');
+    sock.subscribe('dpeAlive');
+    console.log('Subscriber connected to clara SUB port: 7772');
 
-  sock.on('message', function(topic, message) {
-    console.log('received a message related to:', topic, 'containing message:', message);
-  });
-} catch(err) {
-  console.log(err);
+    sock.on('message', function(topic, metadata, data) {
+        var topic = data.toString('binary').split("?")[0];
+        if (!set.has(topic)) {
+            set.add(topic);
+            console.log(new Date().toLocaleString() + ": DPE found", topic);
+        }
+    });
+
+} catch (err) {
+    console.log(err);
 }
