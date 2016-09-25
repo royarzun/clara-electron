9c0605184ddd316ce99826483abb3fe9e9b8125b
Ricardo Oyarzun <royarzun@gmail.com>
2016-09-25 14:58:27 -0400

    Add registration helper

---
+++ clara/data/registration.js
@@ -0,0 +1,26 @@
+const builder = require('protobufjs').loadProtoFile(__dirname + "/proto/xMsgRegistration.proto"),
+    xMsgRegistrationBuilder = builder.build("org.jlab.coda.xmsg.data.xMsgRegistration");
+
+class xMsgRegistration {
+  constructor(buffer){
+    this.register = xMsgRegistrationBuilder.decode(buffer);
+  }
+
+  get name() {
+    return this.register.name;
+  }
+
+  get decription() {
+    return this.register.description;
+  }
+
+  get host() {
+    return this.register.host;
+  }
+
+  get port() {
+    return this.register.port;
+  }
+}
+
+exports.xMsgRegistration = xMsgRegistration;
