71bc399aef14c604b419b32cd2ddd6374e1dbb4f
Ricardo Oyarzun <royarzun@gmail.com>
2016-10-05 01:05:33 -0300

    Nav: Identify first found service

--- clara/renderers/navsidebar-renderer.js
+++ clara/renderers/navsidebar-renderer.js
@@ -7,17 +7,24 @@ var {ipcRenderer} = require('electron');
 var myip = require('quick-local-ip');
 var socket = require('zmq').socket('req');
 
+
 socket.on('message', function () {
     // message, sender, status, data[]
     for (i = 3; i < arguments.length; i++) {
       var reg = new xMsgRegistration(arguments[i]),
           m = reg.name.match(REGEX);
+      var first = true;
+      var first_service = "";
       if (m) {
         var div = document.getElementById('nav-services'),
             button = document.createElement('button');
 
         button.setAttribute('class', 'nav-button');
         button.setAttribute('data-section', 'graphs');
+        if (first) {
+          first_service = 'button-graphs-' + m[0];
+          first = false;
+        }
         button.setAttribute('id', 'button-graphs-' + m[0]);
         button.setAttribute('type', 'button');
         button.appendChild(document.createTextNode(m[0]));
@@ -27,16 +34,17 @@ socket.on('message', function () {
         div.appendChild(button);
       }
     }
+    // Click the first one to take us to the service histogram view;
+    var button = document.getElementById(first_service);
+    button.click();
 });
 
 socket.connect('tcp://' + myip.getLocalIP4() + ":8888");
 socket.send(ALL_SERVICES_MSG);
 
-
 setTimeout(function() {
   socket.unref();
 }, 1000);
-
 
 window.onclosed = function(){
    socket.close();
