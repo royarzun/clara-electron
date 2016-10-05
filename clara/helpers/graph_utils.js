503dc092e3f240bb846d5c2b983f570487a831b8
Ricardo Oyarzun <royarzun@gmail.com>
2016-10-05 16:25:53 -0300

    Add new methods to graph_utils
    Add:
    - JulienDayToUTC
    - iecToUTC
    - iecToUnix
    - yymmddToUTC

--- clara/helpers/graph_utils.js
+++ clara/helpers/graph_utils.js
@@ -47,16 +47,57 @@ function getAxisInfo(units, func, type) {
     }
 }
 
-
 function jq(myid) {
     return "C" + myid.replace(/\(|\)|:|\.|\[|\]|,|\/| |=/g, '');
+}
+
+function julienDayToUTC(jd) {
+    // check for illegal value
+    if (jd == null || jd == 0) return null;
+
+    var unix_zero = 2440587.5,
+        msc = (jd - unix_zero) * 8640000, // in 10ms increments
+        unix_secs = Math.floor(msc + 0.5) / 100;
+
+    return unix_secs * 1000;
+}
+
+function iecToUTC(iec) {
+    // iet is microseconds since 1/1/1958
+    return iec / 1000.0 + iecToUNIX;
 }
 
 function noop(val) {
     return val;
 }
 
+function yymmddToUTC(yymmdd) {
+    // check for illegal value
+    if (yymmdd == null || yymmdd == 0) return null;
+
+    var datestr = yymmdd.toString();
+
+    // going to assume 20xx
+    var year = "20" + datestr.substring(0, 2),
+        month = datestr.substring(2, 4),
+        day = datestr.substring(4, 6),
+        fracday = datestr.substring(6),
+        hours = Math.floor(fracday * 24),
+        frachrs = fracday * 24 - hours,
+        minutes = Math.floor(frachrs * 60),
+        fracmins = frachrs * 60 - minutes,
+        seconds = Math.floor(fracmins * 60),
+        fracsecs = fracmins * 60 - seconds,
+        d = new Date(year, month, day, hours, minutes, seconds, fracsecs * 1000);
+    return d.getTime();
+}
+
+
 exports.chartbox = chartbox;
 exports.getAxisInfo = getAxisInfo;
+exports.iecToUNIX = iecToUNIX;
+exports.iecToUTC = iecToUTC;
 exports.jq = jq;
+exports.julienDayToUTC = julienDayToUTC;
 exports.noop = noop;
+exports.yymmddToUTC = yymmddToUTC;
