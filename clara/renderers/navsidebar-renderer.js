7a55a7720e3af494186834ea9f4111c9147350c5
Ricardo Oyarzun <royarzun@gmail.com>
2016-10-06 14:53:44 -0300

    NAV: Refresh service list every 5 seconds

--- clara/renderers/navsidebar-renderer.js
+++ clara/renderers/navsidebar-renderer.js
@@ -1,5 +1,5 @@
 const REGEX = /([^:_%]+(%([\d]+))?_(java|python|cpp)):\w+:\w+$/g;
-const ALL_SERVICES_MSG = ["allSubscriber", "probeService", "allSubscriber"];
+const ALL_SERVICES_MSG = ['allSubscriber', 'probeService', 'allSubscriber'];
 
 const xMsgRegistration = require('../data/registration.js').xMsgRegistration;
 
@@ -7,49 +7,47 @@ var {ipcRenderer} = require('electron');
 var myip = require('quick-local-ip');
 var socket = require('zmq').socket('req');
 
+var services_set = new Set();
 
-socket.on('message', function () {
+socket.on('message', function() {
     // message, sender, status, data[]
     for (i = 3; i < arguments.length; i++) {
-      var reg = new xMsgRegistration(arguments[i]),
-          m = reg.name.match(REGEX);
-      var first = true;
-      var first_service = "";
-      if (m) {
-        var div = document.getElementById('nav-services'),
-            button = document.createElement('button');
+        var reg = new xMsgRegistration(arguments[i]),
+            filtered_service = reg.name.match(REGEX);
+        if (filtered_service) {
+            if (!services_set.has(filtered_service[0])) {
+                var div = document.getElementById('nav-services'),
+                    button = document.createElement('button');
 
-        button.setAttribute('class', 'nav-button');
-        button.setAttribute('data-section', 'graphs');
-        if (first) {
-          first_service = 'button-graphs-' + m[0];
-          first = false;
+                button.setAttribute('class', 'nav-button');
+                button.setAttribute('data-section', 'graphs');
+                button.setAttribute('id', 'button-graphs-' + filtered_service[0]);
+                button.setAttribute('type', 'button');
+                button.appendChild(document.createTextNode(filtered_service[0]));
+                button.addEventListener('click', function() {
+                    ipcRenderer.send('start-plotting', filtered_service[0]);
+                });
+                div.appendChild(button);
+                services_set.add(filtered_service[0]);
+            }
         }
-        button.setAttribute('id', 'button-graphs-' + m[0]);
-        button.setAttribute('type', 'button');
-        button.appendChild(document.createTextNode(m[0]));
-        button.addEventListener('click', function() {
-          ipcRenderer.send('start-plotting', m[0]);
-        });
-        div.appendChild(button);
-      }
     }
-    // Click the first one to take us to the service histogram view;
-    var button = document.getElementById(first_service);
-    button.click();
 });
 
-socket.connect('tcp://' + myip.getLocalIP4() + ":8888");
-socket.send(ALL_SERVICES_MSG);
+socket.connect('tcp://' + myip.getLocalIP4() + ':8888');
+
+setInterval(function() {
+    socket.send(ALL_SERVICES_MSG);
+}, 5000);
 
 setTimeout(function() {
-  socket.unref();
+    socket.unref();
 }, 1000);
 
-window.onclosed = function(){
-   socket.close();
+window.onclosed = function() {
+    socket.close();
 };
 
 window.onerror = function(error, url, line) {
-   ipcRenderer.send('errorInWindow', error);
+    ipcRenderer.send('errorInWindow', error);
 };
