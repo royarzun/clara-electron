46b23850c0424476f632c4e8e7898682aa2ca721
Ricardo Oyarzun <royarzun@gmail.com>
2016-10-08 10:24:01 -0300

    histograms Demo blocks are created dinamically, resolves #2

--- clara/renderers/histogram-renderer.js
+++ clara/renderers/histogram-renderer.js
@@ -8,35 +8,67 @@ window.onerror = function(error, url, line) {
     ipcRenderer.send('errorInWindow', error);
 };
 
-var subscribers = []
-    // Find all buttons that were created in the step before:
-for (elementId in document.getElementsByClassName('trigger')) {
-    var element = document.getElementById(elementId);
-    if (element) {
-        element.addEventListener('click', function() {
-            var id = this.id.replace('-button', '');
-            if (this.textContent == "Start") {
-                subscribers[id] = setInterval(function() {
-                    child.send(id);
-                }, 5000);
-                this.textContent = "Stop";
-            } else {
-                this.textContent = "Start";
-                clearInterval(subscribers[id]);
-            }
-        });
+function createDemoNodes(names) {
+    var i;
+    var section = document.getElementById('graphs-section');
+    for (i = 0; i < names.length; i++) {
+        var demoText = `
+          <div class="demo">
+            <div class="demo-wrapper">
+              <button id="open-file-demo-toggle" class="js-container-target demo-toggle-button">` + names[i] + `
+                <div class="demo-meta u-avoid-clicks">HighCharts.js</div>
+              </button>
+              <div class="demo-box">
+                <div class="demo-controls">
+                  <button class="demo-button trigger" id="` + names[i] + `-button">Start</button>
+                </div>
+                <div id="` + names[i] + `-graph"></div>
+              </div>
+            </div>
+          </div>`;
+        section.insertAdjacentHTML('beforeend', demoText);
     }
+    require('../../assets/js/demo-btns.js');
 }
+
 
 ipcRenderer.on('histogram-format', (event, args) => {
     //Load from clara/histogram-format the reader for the delivered data
     try {
         histosFormat = require('../histogram-format/' + args);
     } catch (e) {
-        // Default format for now is NAIADS
+        // The next arg after format should the types of graphs to create
+        // This should be an array, for now i just create a known array for the
+        // NAIADS case.
+        createDemoNodes(['h1f', 'h2f', 'p1f', 'p2f']);
+        createTriggers();
+        // Default format for NOW is NAIADS
         histosFormat = require('../histogram-format/naiads');
     }
 });
+
+function createTriggers() {
+    var subscribers = []
+        // Find all buttons that were created in the step before:
+    for (elementId in document.getElementsByClassName('trigger')) {
+        var element = document.getElementById(elementId);
+        if (element) {
+            element.addEventListener('click', function() {
+                var id = this.id.replace('-button', '');
+                if (this.textContent == "Start") {
+                    subscribers[id] = setInterval(function() {
+                        child.send(id);
+                    }, 5000);
+                    this.textContent = "Stop";
+                } else {
+                    this.textContent = "Start";
+                    clearInterval(subscribers[id]);
+                }
+            });
+        }
+    }
+}
+
 
 var child = cp.fork('./clara/renderers/subscriber-renderer');
 child.on('message', function(args) {
