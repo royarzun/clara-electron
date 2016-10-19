6e0b2983f3abff9c81cce8f29e42191bd8cbc1b7
Ricardo Oyarzun <royarzun@gmail.com>
2016-10-18 22:32:51 -0300

    Remove logging from subscriber

--- clara/helpers/subscriber.js
+++ clara/helpers/subscriber.js
@@ -10,7 +10,6 @@ class Subscriber {
         var myip = require('quick-local-ip');
         this.sock.connect('tcp://' + myip.getLocalIP4() + ':7772');
         this.sock.subscribe(this.topic);
-        console.log('Subscriber connected to clara SUB port: 7772');
         this.sock.on('message', callback);
     }
 
